a9045ffd36d4b355ecd19d3614f8340dd67028fa
Chen <phoebec2@illinois.edu>
2021-10-29 00:17:20 -0500

    changed back int_16 to float

--- glasses_ble.ino
+++ glasses_ble.ino
@@ -19,7 +19,7 @@
 #define BLE_UUID_TMP                  "2715"
 #define BLE_UUID_CLR                  "2716"
 
-#define BLE_IMU_BUF_SIZE              122 * 1 //122 samples * 1 2-byte value //20 samples* 3 float values// 6 * 9 nine items
+#define BLE_IMU_BUF_SIZE              60 * 1 //60 samples * 1 4-byte value //20 samples* 3 float values// 6 * 9 nine items
 #define TMP_BUF_SIZE                  1 * 3
 #define CLR_BUF_SIZE                  2 * 5
 #define MIC_BUF_SIZE                  (BLE_MAX_SEND_SIZE / 2) * DOWNSAMPLE_RATE
@@ -29,7 +29,7 @@
 //----------------------------------------------------------------------------------------------------------------------
 
 BLEService testService( BLE_UUID_TEST_SERVICE );
-BLECharacteristic IMUCharacteristic( BLE_UUID_IMU, BLERead | BLENotify , BLE_IMU_BUF_SIZE * sizeof(int16_t), sizeof(int16_t));
+BLECharacteristic IMUCharacteristic( BLE_UUID_IMU, BLERead | BLENotify , BLE_IMU_BUF_SIZE * sizeof(float), sizeof(float));
 BLECharacteristic TMPCharacteristic( BLE_UUID_TMP, BLERead | BLENotify , TMP_BUF_SIZE * sizeof(float), sizeof(float));
 BLECharacteristic CLRCharacteristic( BLE_UUID_CLR, BLERead | BLENotify , CLR_BUF_SIZE * sizeof(int), sizeof(int));
 BLECharacteristic MICCharacteristic(BLE_UUID_MIC, BLERead | BLENotify, BLE_MAX_SEND_SIZE);
