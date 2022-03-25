f19c3bd2db41864d19be6296167bb48b2c48c6a6
Eric <gnodipac886@github.com>
2022-03-25 01:24:15 -0500

    add back deleted libraries, no dep issues

--- src/iGlass.h
+++ src/iGlass.h
@@ -3,10 +3,8 @@
 	#include "api/iGlass_ble_rounded_angle.h"
 	#include "api/iGlass_ble_send_imu.h"
 	#include "hardware/iGlass_ble.h"
-	//.............dep issues #include "hardware/iGlass_imu.h"
-	#include "unittest/iGlass_unittest.h"
 	#include "hardware/iGlass_mic.h"
-	// #include "hardware/iGlass_rgb.h"
+	#include "hardware/iGlass_rgb.h"
 	#include "hardware/iGlass_ir.h"
 	#include "hardware/iGlass_sd.h"
 #endif
